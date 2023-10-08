a47bf9665afc31fa3da6c75776a4963792ee8bd6
Giangualani12 <141842179+Giangualani12@users.noreply.github.com>
2023-10-08 12:31:56 -0300

    Add files via upload

---
+++ ej 6 tp 5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+void sueldos(int*suel) {
+	
+	int i,mas=0,menos=0;
+	for (i=1;i<=20;i++){
+		printf("Ingrese sueldo : %d \n",i);
+		scanf("%d",&suel [i]);
+		
+		
+		
+		
+		if(suel [i] > 2000){
+			mas =mas+1;
+		}
+		else 
+		   menos=menos+1; 
+	}
+	
+	printf("Ganan mas de $2000:  %d \n ",mas);
+	printf("Ganan menos de $2000: %d \n ",menos );
+
+}
+
+int main (){
+	int suel[21];
+	sueldos(suel);
+	system("pause");
+	return 0;
+}
+	
+	
